b7fc5110b41ca7f4380646e65dfddfc3d155129e
kerfmaster <kerfmaster@users.noreply.github.com>
2020-02-10 14:11:19 -0600

    Dinosaur Moves
    the dino moves, but not well.

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -8,33 +8,56 @@ let x , y;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = windowWidth;
 }
 
 function draw() {
   background(0, 150, 255);
   hill();
-  dinosaur();
+  dinosaur(x);
   sun();
+  textSize(32);
+  text("David Rempel", 10, windowHeight - 10);
+  
 }
 
 function hill(){
-  fill( 100 ,200, 0);
+  
+  fill(100 ,200, 0);
   rectMode(CENTER);
-  rect(width / 2, width / 1, windowWidth, windowHeight / .95);
+  rect(width / 2, height / 1, windowWidth, windowHeight / 1);
   rectMode(CORNER);
   
 }
 
 function dinosaur(){
+  
   fill(235, 117, 77);
-  rect(windowWidth / 2, windowHeight / 2.3, windowWidth * 0.011, 40); // neck
-  rect(windowWidth / 2.02, windowHeight / 2.1, windowWidth * 0.009, 25); //leg 1
-  rect(windowWidth / 1.97, windowHeight / 2.1, windowWidth * 0.009, 25); //leg 2
-  rect(windowWidth / 1.92, windowHeight / 2.1, windowWidth * 0.009, 25); //leg 3
-  rect(windowWidth / 1.87, windowHeight / 2.1, windowWidth * 0.009, 25); //leg 4
-  ellipse(windowWidth / 2, windowHeight / 2.3, windowWidth * 0.03, windowHeight * 0.025); //head
-  ellipse(windowWidth / 1.923, windowHeight / 2.09, windowWidth * 0.05  , windowHeight * 0.03); //torso
+  rect(x / 2, windowHeight / 2.3, 10, 40); // neck
+  rect(x / 2.02, windowHeight / 2.1, 7, 25); //leg 1
+  rect(x / 1.97, windowHeight / 2.1, 7, 25); //leg 2
+  rect(x / 1.92, windowHeight / 2.1, 7, 25); //leg 3
+  rect(x / 1.87, windowHeight / 2.1, 7, 25); //leg 4
+  ellipse(x / 2.01, windowHeight / 2.3, 30, 25); //head
+  ellipse(x / 1.923, windowHeight / 2.09, windowWidth * 0.05  , windowHeight * 0.03); //torso
+
 }
+
+
+
+
+
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW){
+    x -=25;
+  }
+  else if (keyCode === RIGHT_ARROW){
+    x += 25;
+  }
+}
+
+
 
 function sun(){
   fill(240, 240, 40);
