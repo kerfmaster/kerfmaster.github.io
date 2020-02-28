7de6c2fad272a0b674947d19f1bf69fa60969153
kerfmaster <kerfmaster@users.noreply.github.com>
2020-02-28 14:10:47 -0600

    Random Grid Colours

--- 08 Loop Grid/sketch.js
+++ 08 Loop Grid/sketch.js
@@ -20,7 +20,7 @@ function circleGrid() {
   for (let y = gridSpacing / 2; y < height; y += gridSpacing) {
     for (let x = gridSpacing / 2; x < width; x += gridSpacing) {
       let size = random(gridSpacing * 0.8, gridSpacing * 1.3);
-      ellipse(x, y, size, gridSpacing);
+      ellipse(x, y, gridSpacing, gridSpacing);
     }
   }
 }
--- Random Colour Squares Assignment/sketch.js
+++ Random Colour Squares Assignment/sketch.js
@@ -9,29 +9,33 @@ let gridSpacing = 30;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  mouseClicked();
+  keyTyped();
+}
+
+function mouseClicked(){
+  rectGrid();
   
-  rkeyTyped();
+  if (keyIsDown(SHIFT)){
+    gridSpacing += 1;
+  }
   
+  else if (gridSpacing !== 5){
+    gridSpacing -= 1;
+  }
 }
 
 function keyTyped(){
   rectGrid();
 }
 
+
 function rectGrid() {
-  for (let x = gridSpacing / 2; x < height; x += gridSpacing) {
-    for (let y = gridSpacing / 2; y < width; y += gridSpacing) {
+  for (let y = 0; y < height; y += gridSpacing) {
+    for (let x = 0; x < width; x += gridSpacing) {
       fill (random(1, 255),random(1, 255),random(1, 255));
       rect(x, y, gridSpacing, gridSpacing);
     }
   }
 }
 
-function draw(){
-  //background(220);
-  //rectGrid();
-}
-
-function mouseClicked(){
-  gridSpacing -= 1;
-}
