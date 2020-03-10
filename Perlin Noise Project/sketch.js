1211d6a530f0c9f7f93f13471bef60c82cfa256c
kerfmaster <kerfmaster@users.noreply.github.com>
2020-03-10 14:10:49 -0600

    flag

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -5,30 +5,40 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let x, y;
-
 let rectWidth = 1;
-
 let yTime = 10;
+
+let tallX;
+let tallY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rectMode(CORNERS);
+  
   strokeWeight(0);
-  keyPressed();
-}
-
-function generateTerrain() {
-  yTime = 10;
-  for (let x = 0; x < width; x) {
-    fill(0); 
-    y = map(noise(yTime), 100, 1, 34, height - 100);
-    rect(x, height, x += 1, random(0,height));
-   
-  }
-}
-
-function keyPressed() {
   background(220);
   generateTerrain();
 }
 
+function generateTerrain() {
+  yTime = 10;
+  tallX = 0;
+  tallY = height;
+  for (let x = 0; x < width; x) {
+    //terrain
+    rectMode(CORNERS);
+    fill(0);
+    let y = map(noise(yTime), 0, 1, 50, height);     
+    rect(x, y, x += rectWidth, height);
+    //flag
+    if (y < tallY){
+      tallY = y;
+      tallX = x;
+    }
+    yTime += 0.0099;  
+  } 
+  drawFlag(tallY,tallX);
+}
+
+function drawFlag(){
+  line(x, y, x, y -30);
+}
