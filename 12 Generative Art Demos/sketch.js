3033d7068880cb764f05d09ddf3d22f2c32cb816
kerfmaster <kerfmaster@users.noreply.github.com>
2020-03-12 14:11:06 -0600

    colors

--- 12 Generative Art Demos/sketch.js
+++ 12 Generative Art Demos/sketch.js
@@ -1,29 +1,37 @@
 // Diaganal Line Art
-
-let gridSpacing = 20;
-const PADDING = 80;
+const RECT_WIDTH = 10;
+const RECT_HEIGHT = 50;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
 }
 
 function draw() {
   background(220);
-  border();
-  drawGrid();
-} 
+  randomSeed(0);
+  drawRowRGB(height*0.2);
+  drawRowHSB(height / 2);
+}
 
-function drawGrid(){
-  for (let x = gridSpacing/2 + PADDING; x < width - PADDING; x += gridSpacing){
-    for (let y = gridSpacing/2 + PADDING; y < height - PADDING; y += gridSpacing){
-      rect(x, y, gridSpacing, gridSpacing);
-    }
+function drawRowRGB(yPos) {
+  colorMode(RGB, 255);
+  for (let x = 0; x < width; x += RECT_WIDTH) {
+    fill(random(255), random(255), random(255));
+    rect(x, yPos, RECT_WIDTH, RECT_HEIGHT);
   }
 }
 
-function border(){
+function drawRowHSB(yPos) {
+  colorMode(HSB, 255);
+  for (let x = 0; x < width; x += RECT_WIDTH) {
+    fill(x / 3 % 360, 300, 300);
+    rect(x, yPos, RECT_WIDTH, RECT_HEIGHT);
+  }
+}
+
+function border() {
   strokeWeight(15);
-  rect(width/2, height/2, width, height);
-  strokeWeight(2); 
+  rect(width / 2, height / 2, width, height);
+  strokeWeight(2);
 }
