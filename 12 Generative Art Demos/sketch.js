3056c45e19965db02ac7e5529a1861a381bd2329
kerfmaster <kerfmaster@users.noreply.github.com>
2020-03-13 14:10:26 -0600

    art

--- 12 Generative Art Demos/sketch.js
+++ 12 Generative Art Demos/sketch.js
@@ -1,6 +1,7 @@
 // Diaganal Line Art
 const RECT_WIDTH = 10;
 const RECT_HEIGHT = 50;
+let colors = ["#ECD078","#D95B43","#C02942","#542437","#53777A"]; //fill with HEX codes as strings
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,7 +12,8 @@ function draw() {
   background(220);
   randomSeed(0);
   drawRowRGB(height*0.2);
-  drawRowHSB(height / 2);
+  drawRowHSB(height*0.5);
+  drawRowCustom(height*0.7);
 }
 
 function drawRowRGB(yPos) {
@@ -22,10 +24,19 @@ function drawRowRGB(yPos) {
   }
 }
 
+function drawRowCustom(yPos){
+  colorMode(RGB,255);
+  for (let x = 0; x < width; x += RECT_WIDTH) {
+    let index = int(random(colors.length));
+    fill(colors[index]);
+    rect(x, yPos, RECT_WIDTH, RECT_HEIGHT);
+  }
+}
+
 function drawRowHSB(yPos) {
   colorMode(HSB, 255);
   for (let x = 0; x < width; x += RECT_WIDTH) {
-    fill(x / 3 % 360, 300, 300);
+    fill(x / 3 % 360, 300, map(mouseY, 0, height, 0 ,300));
     rect(x, yPos, RECT_WIDTH, RECT_HEIGHT);
   }
 }
---
+++ Generative Art Warm-up/sketch.js
@@ -0,0 +1,22 @@
+// Generative Art Warm-Up
+// David Rempel 
+// 3/13/2020
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x, y;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function rects() {
+  x = width / 2;
+  y = height / 2;
+  
+}
